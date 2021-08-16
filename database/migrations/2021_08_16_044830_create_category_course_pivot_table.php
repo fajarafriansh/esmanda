f480d21f0b79100bdc4276ec19ec69d1182745a1
Fajar <fajar.afrianshah@gmail.com>
2021-08-16 13:34:22 +0700

    change category course pivot table name

--- database/migrations/2021_08_16_044830_create_category_course_pivot_table.php
+++ database/migrations/2021_08_16_044830_create_category_course_pivot_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCourseCategoryPivotTable extends Migration
+class CreateCategoryCoursePivotTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCourseCategoryPivotTable extends Migration
      */
     public function up()
     {
-        Schema::create('course_category', function (Blueprint $table) {
+        Schema::create('category_course', function (Blueprint $table) {
             $table->unsignedBigInteger('course_id');
             $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
             $table->unsignedBigInteger('category_id');
@@ -28,6 +28,6 @@ class CreateCourseCategoryPivotTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('course_category');
+        Schema::dropIfExists('category_course');
     }
 }

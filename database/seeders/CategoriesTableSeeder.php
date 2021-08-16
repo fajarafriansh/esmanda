7695957e2cd180370a7bfa023a1102b54bf5d0bc
Fajar <fajar.afrianshah@gmail.com>
2021-08-16 13:35:48 +0700

    create category table seeder

---
+++ database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+            [
+                'id' => 1,
+                'name' => 'Uncategories',
+            ]
+        ];
+
+        Category::insert($categories);
+    }
+}

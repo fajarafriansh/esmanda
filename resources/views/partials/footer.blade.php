052d66476b8720f6ab09eda5b0e46591c48d7774
Fajar <fajar.afrianshah@gmail.com>
2021-08-16 11:41:45 +0700

    fix footer logo

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,10 @@
 
   <div class="container">
     <div class="row">
-      <div class="col-12 col-sm-12 col-lg-6 mb-4 order-0 order-sm-0"><a class="text-decoration-none" href="#"><img src="img/gallery/footer-logo.png" height="51" alt="" /></a>
+      <div class="col-12 col-sm-12 col-lg-6 mb-4 order-0 order-sm-0">
+        <a class="text-decoration-none" href="{{ route('home') }}">
+          <img src="{{ asset('img/gallery/footer-logo.png') }}" height="51" alt="" />
+        </a>
         <p class="text-light my-4"> <i class="fas fa-map-marker-alt me-3"></i><span class="text-light">1500 Treat Ave, Suite 200  &nbsp;</span><a class="text-light" href="tel:+604-680-9785">+604-680-9785</a><br />San Francisco, CA 94110</p>
         <p class="text-light"> <i class="fas fa-envelope me-3"> </i><a class="text-light" href="mailto:vctung@outlook.com">vctung@outlook.com </a></p>
         <p class="text-light"> <i class="fas fa-phone-alt me-3"></i><a class="text-light" href="tel:1-800-800-2299">1-800-800-2299 (Support)</a></p>
